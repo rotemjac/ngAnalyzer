81170ffa0502fad0bd902e66f7c5c4e5244c90e3
ET-CS <et.programming@gmail.com>
2015-02-20 02:45:46 +0200

    New design concept

--- 0App--ngAnalyzer/mainApp/Directives/myTabs2.js
+++ 0App--ngAnalyzer/mainApp/Directives/myTabs2.js
@@ -13,22 +13,7 @@
 
         var directiveDefinitionObject = {
             restrict: 'E',
-            template:
-            '<nav style="background-color: black">'+
-            '<div class="navbar-header">'+
-            '    <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">'+
-            '    <span class="sr-only"></span>'+
-            '    <span class="icon-bar"></span>'+
-            '    <span class="icon-bar"></span>'+
-            '    <span class="icon-bar"></span>'+
-            '    </button>'+
-            '    <a href="#" class="navbar-brand"></a>'+
-            '</div>'+
-            '<div id="navbarCollapse" class="collapse navbar-collapse">'+
-            '    <ul class="nav navbar-nav">'+
-            '    </ul>'+
-            '</div>'+
-            '</nav>',
+            templateUrl: 'Templates/nav.html',
             replace: true,
             controller: function(){
             },
@@ -40,11 +25,11 @@
 
                 var tabsData = mainCtrl.navData;
 
-                var elem = iElement.find('.navbar-nav');
+                var elem = iElement.find('.nav');
                 for (var i=0 ; i<tabsData.length; i++) {
-                    elem.append('<li> ' +
+                    elem.append('<li class="navitem"> ' +
                                       '<a ng-click="mainCtrl.changeTab($event)">'  +
-                                            '<span style="color: #767676"' +getGlyphiconClass(tabsData[i])+ '>'+ tabsData[i] +'</span>'  +
+                                            '<span ' +getGlyphiconClass(tabsData[i])+ '/> <span>' + tabsData[i] +'</span>'  +
                                       '</a>' +
                                 '</li>');
                 }
